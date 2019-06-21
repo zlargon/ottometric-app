2d5f1c4515eb315814e463fa8fccc300a1b79161
zlargon <zlargon1988@gmail.com>
2019-06-20 23:16:36 -0400

    a lot of update

--- src/App.js
+++ src/App.js
@@ -1,47 +1,113 @@
-import React from 'react';
-import GoogleMapReact from 'google-map-react';
-import Camera from './Camera';
+import React, { Component } from 'react';
+import { GoogleMap, Polyline, withGoogleMap, withScriptjs } from 'react-google-maps';
+import openSocket from 'socket.io-client';
+import * as util from './utils/map';
 import './App.css';
 
-const Marker = ({ text }) => <div className="marker">{text}</div>;
+const MapWithPolyline = withScriptjs(withGoogleMap(props => (
+  <GoogleMap
+    defaultZoom={20}
+    center={props.currentLatLng}
+  >
+    <Polyline path={props.polylinePath} />
+  </GoogleMap>
+)));
 
-const App = () => {
-  return (
-    <div className="container">
-      <header>
-        <h2>OTTOMETRIC</h2>
-      </header>
-      <section>
-        <div className="google-map">
-          <GoogleMapReact
-            bootstrapURLKeys={{ key: 'AIzaSyD6qTu55F0bSv_Bj05mxZ8ffvxyP2rzxO0' }}
-            defaultCenter={{lat: 42.3418206, lng: -71.091735}}
-            defaultZoom={11}>
-
-            <Marker lat={42.3418206} lng={-71.091735} text="my position" />
-          </GoogleMapReact>
-        </div>
-        <aside>
-          <div className="lidar">
-            lidar
+export default class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.container = React.createRef();
+    this.positions = [];
+    this.socket = openSocket('http://localhost:6999');
+    this.fileIndex = 0;
+    this.geometry = util.generateGeometry();
+  }
+  state = {
+    polylinePath: [],
+    currentLatLng: { lat: 49.011212804408, lng: 8.4228850417969 },
+  }
+  componentDidMount() {
+    const map = util.generateScene();
+    this.container.current.appendChild(map.renderer.domElement);
+    const animate = () => {
+      requestAnimationFrame(animate);
+      map.renderer.render(map.scene, map.camera);
+    };
+    const updateFrame = () => new Promise(() => {
+      if (this.lastPointCloud) {
+        map.scene.remove(this.lastPointCloud);
+      }
+      this.lastPointCloud = util.generatePointCloud(this.geometry);
+      this.geometry.dispose();
+      this.geometry = util.generateGeometry();
+      map.scene.add(this.lastPointCloud);
+    });
+    const socketEnd = (geolocation) => {
+      // console.log(geolocation);
+      updateFrame();
+      const currentLatLng = { lat: Number(geolocation[0]), lng: Number(geolocation[1]) };
+      const polylinePath = [...this.state.polylinePath, currentLatLng];
+      this.setState({ currentLatLng, polylinePath });
+      if (!this.pause) {
+        this.nextShot();
+      }
+    };
+    const socketPosition = (positionArr) => {
+      const vectArray = util.transferArrayBufferToVect(positionArr);
+      util.pushDataToGeometry(this.geometry, vectArray);
+    };
+    animate();
+    // this.socket.on('start', () => { this.setState({ loading: true }); });
+    this.socket.on('position', socketPosition);
+    this.socket.on('end', socketEnd);
+  }
+  nextShot = () => {
+    let newIndex = this.fileIndex + 1;
+    if (newIndex > 153) {
+      newIndex = 0;
+      this.setState({ polylinePath: [] });
+    }
+    this.pause = false;
+    this.fileIndex = newIndex;
+    this.socket.emit('getPosition', newIndex);
+  }
+  togglePause = () => {
+    this.pause = true;
+  }
+  render() {
+    return (
+      <div className="app">
+        <header>
+          <h2>OTTOMETRIC</h2>
+        </header>
+        <main>
+          <div className="googlemap">
+            <MapWithPolyline
+              googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=drawing&key=AIzaSyD6qTu55F0bSv_Bj05mxZ8ffvxyP2rzxO0"
+              loadingElement={<div />}
+              containerElement={<div style={{ width: '100%', height: '100%' }} />}
+              mapElement={<div style={{ width: '100%', height: '100%' }} />}
+              currentLatLng={this.state.currentLatLng}
+              polylinePath={this.state.polylinePath}
+            />
           </div>
-          <div className="aside-down">
-            <div className="camera">
-              <Camera
-                speed={100}
-                imageFolder="/2011_09_26/2011_09_26_drive_0052_sync/image_00/data/" />
-            </div>
-            <div className="information">
-              information
-            </div>
-          </div>
-        </aside>
-      </section>
-      <footer>
-        footer
-      </footer>
-    </div>
-  );
+          <section>
+            <div id="lidar" ref={this.container}></div>
+            <aside>
+              <div className="camera">
+                <div>no image</div>
+              </div>
+              <div className="information">
+                information
+              </div>
+            </aside>
+          </section>
+        </main>
+        <footer>
+          <button onClick={this.nextShot}>play</button>
+          <button onClick={this.togglePause}>pause</button>
+        </footer>
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/utils/map.js
@@ -0,0 +1,92 @@
+import * as THREE from 'three';
+import * as d3 from 'd3';
+import Orbitcontrols from 'three-orbitcontrols';
+
+const chunkSize = 4;
+
+const color = d3.scaleLinear().domain([0, 0.6]).range([d3.rgb(0, 255, 0), d3.rgb(255, 0, 0)]);
+const defaultColor = new THREE.Color('skyblue');
+
+export const transferArrayBufferToVect = (arrBuffer) => {
+  const floatArr = new Float32Array(arrBuffer);
+  const arr = [];
+  for (let i = 0; i < floatArr.length - 1; i += chunkSize) {
+    const z = floatArr[i];
+    const x = floatArr[i + 1];
+    const y = floatArr[i + 2];
+    // const w = ((x ** 2) + (y ** 2) + (z ** 2)) ** 0.5;
+    const vect = {
+      x, y, z,
+    };
+    arr.push(vect);
+  }
+  return arr;
+};
+
+export const generateGeometry = () => {
+  return new THREE.Geometry();
+};
+
+export const pushDataToGeometry = (geometry, data) => {
+  const vertices = data.map(d => new THREE.Vector3(d.x, d.y, d.z));
+  const colors = Array(data.length).fill(defaultColor);
+  geometry.vertices = geometry.vertices.concat(vertices);
+  geometry.colors = geometry.colors.concat(colors);
+  // data.forEach((d) => {
+  //   geometry.vertices.push(new THREE.Vector3(d.x, d.y, d.z));
+  //   geometry.colors.push(defaultColor);
+  // });
+};
+
+export const generatePointCloud = (geometry) => {
+  // console.log(data);
+  const pointMaterial = new THREE.PointsMaterial({ size: 2, sizeAttenuation: false, vertexColors: THREE.VertexColors });
+  const pointCloud = new THREE.Points(geometry, pointMaterial);
+  return pointCloud;
+};
+
+export const generateScene = () => {
+  const ele = document.getElementById('lidar');
+  const scene = new THREE.Scene();
+  const SCREEN_WIDTH = ele.clientWidth;
+  const SCREEN_HEIGHT = ele.clientHeight;
+  // camera attributes
+  const VIEW_ANGLE = 45;
+  const ASPECT = SCREEN_WIDTH / SCREEN_HEIGHT;
+  const NEAR = 1;
+  const FAR = 10000;
+
+  // set up camera
+  const camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
+  camera.position.set(0, 100, 0);
+  camera.lookAt(new THREE.Vector3(0, 0, 0));
+  scene.add(camera);
+
+  // the camera defaults to position (0,0,0)
+  // so pull it back (z = 400) and up (y = 100) and set the angle towards the scene origin
+  const axes = new THREE.AxesHelper(50);
+  scene.add(axes);
+
+  // const grid = new THREE.GridHelper(100, 100, 1);
+  // scene.add(grid);
+
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+
+  // target position
+  // const box = new THREE.Box3();
+  // box.setFromCenterAndSize(new THREE.Vector3(5, -1, -2.5), new THREE.Vector3(2, 2, 5));
+  // const helper = new THREE.Box3Helper(box, 0xffff00);
+  // scene.add(helper);
+
+  const orbitControls = new Orbitcontrols(camera, renderer.domElement);
+  const object = {
+    scene,
+    camera,
+    renderer,
+    orbitControls,
+  };
+  window.THREE = THREE;
+  window.q = object;
+  return object;
+};
