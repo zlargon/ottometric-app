9553db26b8baad174f23051ca603cf0437575c7a
zlargon <zlargon1988@gmail.com>
2019-06-21 06:16:18 -0400

    Updates

---
+++ precache-manifest.ac2f81e3c82010f7aef8c9a1b435fdcb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe5ce6bbd7f749659428730ef639c545",
+    "url": "/ottometric-app/index.html"
+  },
+  {
+    "revision": "a015d761f847b66a0eec",
+    "url": "/ottometric-app/static/css/main.a957f0c4.chunk.css"
+  },
+  {
+    "revision": "e96b5c83af4751e19948",
+    "url": "/ottometric-app/static/js/2.9b38482c.chunk.js"
+  },
+  {
+    "revision": "a015d761f847b66a0eec",
+    "url": "/ottometric-app/static/js/main.f30eb0de.chunk.js"
+  },
+  {
+    "revision": "17ce1f1f2be9e061f788",
+    "url": "/ottometric-app/static/js/runtime~main.b2c5de89.js"
+  }
+]);
